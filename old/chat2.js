a84978ad40fc4b27695ef0c7f38ab6e6e2cf2948
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2025-01-09 02:13:50 +0300

    Update chat2.js

--- old/chat2.js
+++ old/chat2.js
@@ -203,7 +203,7 @@
       right: 10px;
       width: 400px;
       height: 415px;
-      background-color: #f5f5f5;
+      background-color: #ffffff5c;
       border: 1px solid #ddd;
       border-radius: 10px;
       box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
